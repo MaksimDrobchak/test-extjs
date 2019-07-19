7874351e6944078ef03229472835eb59b99ec8ba
Maksim <maksim.drobchak@kenshoo.com>
2019-07-19 17:43:30 +0300

    save

--- Classes/TestPanel.js
+++ Classes/TestPanel.js
@@ -1,6 +1,12 @@
 Ext.define('Classes.TestPanel', {
   alias: 'widget.testpanel',
   extend: 'Ext.panel.Panel',
+  id: 'testtestpanel',
+  width: 300,
+  height: 230,
+  padding:10,
+  title: 'Основной контейнер',
+  layout: 'fit',
   title: 'Test Panel',
-  html: '<div><h1>Test Panel</h1></div>'
+  html: '<div><h1>Test Panel</h1></div>',
 });
--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-
 Ext.require('Classes.TestPanel');
 Ext.application({
   name: 'HelloExt',
   launch: function() {
       Ext.create('Ext.container.Viewport', {
-          layout: 'fit',
+        layout: 'fit',
           items: [
-              {
-                  xtype: 'testpanel',
-                  id: 'testpanel',
-                  items: [
+            Ext.create('Ext.Panel', {
+              title: 'Таблица',
+              width: 500,
+              height: 100,
+              padding: 10,
+              layout:'column',
+              items: [
                   {
-                    html: `
-                    <ul id="menu">
-                      <li class='buy'>Купить</li>
-                      <li class='sell'>Продать</li>
-                      <li class='exit'>Выход</li>
-                    </ul>
-                    `
+                      xtype: 'panel',
+                      title: 'Первый столбец',
+                      html: '<span class="buy">Поле 1</span>',
+                      width: 200,
+                      margin: '25'
                   },
                   {
-                    html: '<span class="selects">HTML_3</span>'
+                      xtype: 'panel',
+                      title: 'Второй столбец',
+                      html: '<span class="buselly">Поле 2</span>',
+                      columnWidth:.4,
+                      margin: '25'
                   },
                   {
-                    html: '<span class="selects">HTML_2</span>',
-                  }]
-              }]
+                      xtype: 'panel',
+                      title: 'Третий столбец',
+                      html: '<span class="exit">Поле 3</span>',
+                      columnWidth:.6,
+                      margin: '25'
+                  }],
+                  renderTo: Ext.get('testtestpanel'),
+          })]
       });
   }
 });
